e19322fe4ff87984b8c039f758bd7aaf6b633ea4
Erhan KESKEN <erhan@kesken.org>
2012-05-12 12:04:08 +0300

    added empty teknoloji-secimi.mm file

---
+++ tr/teknoloji-secimi.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1336813329233" ID="ID_985795993" MODIFIED="1336813346363" TEXT="teknoloji secimi"/>
+</map>
